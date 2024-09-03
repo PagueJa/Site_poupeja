7cad65f1ebddbc72ab5db5b1d7137aa614498e35
Victor Gabriel de Freitas Passos <victor.passos@dinfor.uniceub>
2024-09-03 10:22:56 -0300

    tabela transacao

--- src/components/layout/RecentTransactions.js
+++ src/components/layout/RecentTransactions.js
@@ -1,11 +1,54 @@
-function RecentTransactions({ items }) {
-    return (
-        <>
-            {items.map((item, index) => (
-                <p key={index}>{item.transacao}</p>
-            ))}
-        </>
-    );
-}
+import React from 'react';
+import styles from './RecentTransactions.module.css';
 
-export default RecentTransactions;
+const transactions = [
+  {
+    id: 1,
+    title: 'Imposto de renda',
+    amount: 590.90,
+    date: '20/10/2002',
+    category: 'Imposto',
+  },
+  {
+    id: 2,
+    title: 'Uber',
+    amount: -20.00,
+    date: '18/10/2002',
+    category: 'Transporte',
+  },
+];
+
+const RecentTransaction = () => {
+  return (
+    <div className='td' style={{ margin: '20px' }}>
+      <table style={{ width: '100%', borderCollapse: 'collapse' }}>
+        <thead >
+          <tr>
+            <th>Transação</th>
+            <th>Sobre</th>
+            <th>Data</th>
+            <th>Categoria</th>
+          </tr>
+        </thead>
+        <tbody>
+          {transactions.map((transaction) => (
+            <tr
+              key={transaction.id}
+              style={{
+                backgroundColor: transaction.amount > 0 ? '#d4edda' : '#f8d7da',
+              }}
+            >
+              <td>{transaction.title}</td>
+              <td>{`R$ ${transaction.amount.toFixed(2)}`}</td>
+              <td>{transaction.date}</td>
+              <td>{transaction.category}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default RecentTransaction;
+
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -7,16 +7,13 @@ const Home = () => {
     const receita = 10000;
     const despesas = 2000; 
 
-    const meusItens = [
-        {trasacao: "Sergio", sobre: "R$ 590,00", data: "20/1/2024", categoria: "empréstimo"},
-        {trasacao: "IPVA", sobre: "R$ 590,00", data: "21/1/2024", categoria: "gastos mensais"}
-    ]
+
 
     return (
         <div className={styles.container}>
             <DoughnutChart className={styles.doughnutChart} receita={receita} despesas={despesas} />
             <h1 className={styles.trasacoes_recentes}>Transações Recentes</h1>
-            <RecentTransactions itens={meusItens}/>
+            <RecentTransactions/>
         </div>
     );
 };
